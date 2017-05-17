171b1e1e8c76c03b76eb7ea05324532fed34004f
yeseg11 <yeseg11@gmail.com>
2017-05-17 10:25:29 +0300

    RED

--- test/test.js
+++ test/test.js
@@ -3,6 +3,9 @@ var C = require('../cash.js');  // our module
 
 describe('Cash Register', function(){
   describe('Module C', function(){
+    it('getChange(210,300) should equal [50,20,20]', function(){
+    assert.deepEqual(C.getChange(210,300), [50,20,20]);
+})
     it('should have a getChange Method', function(){
       assert.equal(typeof C, 'object');
       assert.equal(typeof C.getChange, 'function');
