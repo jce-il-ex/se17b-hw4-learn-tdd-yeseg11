0263a0b2a592fd944bad3c1ca94969bcd06db66a
yeseg11 <yeseg11@gmail.com>
2017-05-17 12:59:41 +0300

    REFACTOR

--- test/test.js
+++ test/test.js
@@ -1,56 +1,19 @@
-var assert = require("assert"); // core module
+var assert = require("assert"); // node.js core module
 var C = require('../cash.js');  // our module
-
-describe('Cash Register', function(){
+describe('Array', function()
+{
   it('getChange(210,300) should equal [50,20,20]', function(){
-      assert.deepEqual(C.getChange(210,300), [50,20,20]);
-  })
-  it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
-      assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
-  })
-  it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
+    assert.deepEqual(C.getChange(210,300), [50,20,20]);
+});
+it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
+    assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
+});
+it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
     assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
 });
-
-describe('Module C', function(){
-    
-    it('should have a getChange Method', function(){
-      assert.equal(typeof C, 'object');
-      assert.equal(typeof C.getChange, 'function');
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so indexOf returns -1
     })
   })
-});  
-C.getChange = function (totalPayable, cashPaid) {
-    'use strict';
-    return [50, 20, 20];    // just enough to pass :-)
-};
-C.getChange = function (totalPayable, cashPaid) {
-    'use strict';
-    if(totalPayable == 486 && cashPaid == 1000)
-        return [500, 10, 2, 2];
-    else if(totalPayable == 210 && cashPaid == 300)
-        return [50, 20, 20];
-};
-var C = {};     // C Object simplifies exporting the module
-C.coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
-C.getChange = function (totalPayable, cashPaid) {
-    'use strict';
-    var change = [];
-    var length = C.coins.length;
-    var remaining = cashPaid - totalPayable;          // we reduce this below
-
-    for (var i = 0; i < length; i++) { // loop through array of notes & coins:
-        var coin = C.coins[i];
-
-        if(remaining/coin >= 1) { // check coin fits into the remaining amount
-            var times = Math.floor(remaining/coin);        // no partial coins
-
-            for(var j = 0; j < times; j++) {     // add coin to change x times
-                change.push(coin);
-                remaining = remaining - coin;  // subtract coin from remaining
-            }
-        }
-    }
-    return change;
-};
-module.exports = C;            // export the module with a single method
+});
