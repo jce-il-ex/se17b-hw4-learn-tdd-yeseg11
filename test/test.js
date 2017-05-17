9e0ac3a34356402d5426e9e7e1fd5d4454ed173a
yeseg11 <yeseg11@gmail.com>
2017-05-17 10:14:06 +0300

    RED

---
+++ test/test.js
@@ -0,0 +1,14 @@
+/*
+var assert = require("assert"); // node.js core module
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so indexOf returns -1
+    })
+  })
+});
+*/
+var C = require('../cash.js');
+
+var coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
